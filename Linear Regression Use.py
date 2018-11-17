431e2863bfbfa6b33fd8a7a7166cde86fde3a70b
Trinfear <Anthony.Alexander117@gmail.com>
2018-11-17 13:29:46 -0700

    added in use case and from scratch for Linear Regression

--- Linear Regression Use.py
+++ Linear Regression Use.py
@@ -2,14 +2,30 @@
 import numpy as np
 from sklearn.linear_model import LinearRegression
 import matplotlib.pyplot as plt
-plt.style.use('fivethirtyeight')
+import random
+plt.style.use('ggplot')
 
-x_train = np.array([[1, 1], [1, 2], [2, 2], [2, 3]])
-y_train = np.multiply(1.3, x_train) + 3
+
+def create_data_set(size, variance, slope):
+    x = []
+    y = []
+    for i in range(size):
+        x.append(i)
+        y.append(i * slope + random.randrange(-variance, variance))
+
+    x = np.array(x, dtype=np.float64)
+    y = np.array(y, dtype=np.float64)
+
+    return x, y
+
+
+x_train, y_train = create_data_set(50, 12, 3)
+
 
 clf = LinearRegression()
-clf.fit(x_train, y_train)
+clf.fit(x_train.reshape(-1, 1), y_train.reshape(-1, 1))
 
-plt.scatter(x_train, y_train)
-plt.plot(x_train, y_train)
+plt.scatter(x_train, y_train, color='r', label='data')
+plt.plot(clf.predict(x_train.reshape(-1, 1)), color='c', label='Line of best fit')
+plt.legend(loc=4)
 plt.show()
