01e55afb786961b935668e5888b078e14451ca94
Trinfear <31570643+Trinfear@users.noreply.github.com>
2018-12-30 17:00:43 -0700

    Added draw_tree function

--- Decision Tree From Scratch.py
+++ Decision Tree From Scratch.py
@@ -1,68 +1,6 @@
 #!python3
 # decision tree from scratch
 
-'''
-
-node object
-    contains a divider
-    contains method to divide incoming data
-    contains two children which are nodes or leaves
-    contains a print tree method:
-
-string question object??
-
-numerical question object??
-        
-
-leaf object
-    contains an answer
-    contains a probability estimate for each type
-    contains a method to generate the answer and probability dictionary
-    doesn't really need to be an object, just a dictionairy the tree tacks on to the end of nodes?
-
-
-generate question
-    intakes a set of data
-    find the answer which creates the most information gain
-    returns answer
-
-get gini
-    finds the probability of getting a random guess correctly given a selection of data
-    do this waited by one type being more common or assume all answers equally likely?
-    gini = 1 - (num most likely response / total num data)
-
-get adjusted gini
-    intake two data sets
-    get gini for both
-    get average gini
-    return average gini
-
-generate tree
-    intakes a dataset
-    calculates gini
-    gets question
-    if no decrease in gini is possible,  generate a leaf node and return it
-    if optimal question decreases gini, generate a node
-    split data
-    call generate tree on two datapoints, and set as children to current node
-    return current node
-
-test
-    find all leaves and find the average gini?
-    just print the probability spread of each leaf, assuming a small number of leaves?
-
-data
-    what will the data look like?
-    label will be a string and always the first item
-    values/features will be an array/ list of several values
-    question picks one of the values at random and gets the type
-    if the type is numerical then create a >= divider
-    if the type is string then create a == divider
-    if another type just return an error for now
-    generate data procedurally or just right it out?
-
-'''
-
 import numpy as np
 from collections import Counter
 
@@ -93,11 +31,19 @@ class Node():
                 answer = self.child_two.predict(features)
         return answer
 
-    def draw_tree(self):
+    def draw_tree(self, jumps):
         # intake how many tabs
         # print that number of tabs and then the divider
         # send to both children with number of tabs plus 1
-        pass
+        branch = ''
+        for jump in range(jumps):
+            branch += '\t'          # this seems like an unclean way to do this
+        branch += str(self.divider)
+        print(branch)
+        if self.child_one:
+            self.child_one.draw_tree(jumps+1)
+        if self.child_two:
+            self.child_two.draw_tree(jumps+1)
 
 
 class Leaf():
@@ -115,8 +61,12 @@ class Leaf():
         return self.probabilities
     
 
-    def draw_tree():
-        pass
+    def draw_tree(self, jumps):
+        branch = ''
+        for jump in range(jumps):
+            branch += '\t'          # this seems like an unclean way to do this
+        branch += str(self.probabilities)
+        print(branch)
 
 
 def get_gini(data):
@@ -227,7 +177,7 @@ new_fruit = [2, 'red', 'round']
 
 classifier = generate_tree(data_set)
 print(classifier.predict(new_fruit))
-##classifier.draw_tree()
+classifier.draw_tree(0)
 
 
 
