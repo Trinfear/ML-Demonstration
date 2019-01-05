75de518400feefd82fd591e9897c9a5361547bda
Trinfear <31570643+Trinfear@users.noreply.github.com>
2019-01-05 13:41:42 -0700

    added RNN

--- Neural Networks Use.py
+++ Neural Networks Use.py
@@ -8,20 +8,18 @@ example cases for several common types of neural networks using keras
 demonstrate the creation of several different models in keras
     standard
     CNN
-
-RNN in the form of LSTM cells will be done in a different project
-as there are no suitable keras datasets and an outside one will
-need to be taken in and processed
+    LSTM
 
 '''
 import time
 import keras
 import numpy as np
 import tensorflow as tf
+import matplotlib.pyplot as plt
 from keras.models import Sequential
 from keras.callbacks import TensorBoard
 from tensorflow.keras.datasets import mnist
-from keras.layers import Dense, Dropout, Flatten, Conv2D, MaxPooling2D
+from keras.layers import Dense, Dropout, Flatten, Conv2D, MaxPooling2D, CuDNNLSTM
 
 
 tensorboard = TensorBoard(log_dir="NNuse_logs/example_network_{}".format(int(time.time())))
@@ -44,7 +42,7 @@ def run_nn():       # direct, feed forward, fully connected neural network
 
     model.add(Dense(10, activation='softmax'))
 
-    model.compile(optimizer='adam',
+    model.compile(optimizer='Adam',
                   loss='sparse_categorical_crossentropy',
                   metrics=['accuracy'])
 
@@ -82,7 +80,7 @@ def run_cnn():      # examins groups of input at a time, effective in image reco
     model.add(Flatten())
     model.add(Dense(10, activation='softmax'))
 
-    model.compile(optimizer='adam',
+    model.compile(optimizer='Adam',
                   loss='sparse_categorical_crossentropy',
                   metrics=['accuracy'])
 
@@ -91,6 +89,47 @@ def run_cnn():      # examins groups of input at a time, effective in image reco
 
     val_loss, val_acc = model.evaluate(x_test, y_test)
     print(val_loss, val_acc)
+
+
+# RNN
+
+def run_rnn():
+
+    (x_train, y_train), (x_test, y_test) = mnist.load_data()
+    
+    x_train = x_train/255       # normalize the data
+    x_test = x_test/255
+    
+    model = Sequential()
+
+    model.add(CuDNNLSTM(128, input_shape=(x_train.shape[1:]), return_sequences=True))
+    # use LSTM not CuDNNLSTM if gpu/cuda are not enabled/present on the machine
+    model.add(Dropout(0.2))
+
+    model.add(CuDNNLSTM(128))
+    model.add(Dropout(0.2))
+
+    model.add(Dense(32, activation='relu'))
+    model.add(Dropout(0.2))
+
+    model.add(Dense(10, activation='softmax'))
+
+    opt = tf.keras.optimizers.Adam(lr=1e-3, decay=1e-5)
+
+    model.compile(loss='sparse_categorical_crossentropy',
+                  optimizer='Adam',
+                  metrics=['accuracy'])
+
+    model.fit(x_train, y_train, verbose=0, callbacks=[tensorboard])
+    # this also insists on being verbose
+
+    val_loss, val_acc = model.evaluate(x_test, y_test)
+    print(val_loss, val_acc)
+
+
+run_nn()
+run_cnn()
+run_rnn()
 
 
 
